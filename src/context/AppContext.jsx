cd0435d4676f3077d138980b5f0787438c4be829
prashanth <prashanth.art2001@gmail.com>
2025-03-23 15:19:15 +0530

    changes

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+
+export const AppContext = createContext(); 
 
 const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AppContextProvider } from "./context/AppContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import AppContextProvider from "./context/AppContext.jsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
